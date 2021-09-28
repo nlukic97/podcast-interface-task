a75de4750c7072ff2a5f0593fceea5e7ce38b624
Nikola Lukic <nlukic97@gmail.com>
2021-09-28 19:29:49 +0200

    javascript completed and optimized

--- starter-code/js/script.js
+++ starter-code/js/script.js
@@ -1,22 +1,12 @@
 // Event listener
 document.getElementById('submit').addEventListener('click',function(){
-    validateEmail()
-})
-
-//validation
-function validateEmail(){
+    
     let val = document.getElementById('email').value
     let regex = new RegExp(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,'g')
 
-    let isEmail = (regex.test(val)) ? true : false;
+    let classListAction = (regex.test(val)) ? 'remove' : 'add'; //remove or add the class to the element classList, based on the RegExp result
 
-}
+    let element = document.querySelector('.error-message')
+    element.classList[classListAction]('show-error')
+})
 
-function toggleError(itemSelector,errorClass){
-    let item = document.querySelector(itemSelector)
-    if(isEmail === true){
-        //hide error
-    } else {
-        //show error
-    }
-}
